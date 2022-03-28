d9205be457edd8049e0efbf280ba495871814502
NWR57 <76746289+NWR57@users.noreply.github.com>
2022-03-28 21:58:41 +0600

    Added Initial Files

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client({ intents: 26737});
+
+require('dotenv').config()
+
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
